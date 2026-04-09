8f54c73aedb31d0350f3609ca1ec0356f0c4599b
FSSLL <almurekhe@gmail.com>
2026-04-09 15:30:11 +0300

    i18n: revert to English-only, remove language switcher

--- hooks/useT.ts
+++ hooks/useT.ts
@@ -1,13 +1,9 @@
-import { usePlayerStore } from '../store/playerStore';
-import { translate, type Language } from '../lib/i18n';
+import { translate } from '../lib/i18n';
 
 export function useT() {
-  const language = usePlayerStore((s) => s.language);
-  return (key: string, vars?: Record<string, string | number>) =>
-    translate(language as Language, key, vars);
+  return (key: string, vars?: Record<string, string | number>) => translate(key, vars);
 }
 
 export function useIsRTL(): boolean {
-  const language = usePlayerStore((s) => s.language);
-  return language === 'ar';
+  return false;
 }
